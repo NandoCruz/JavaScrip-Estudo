3838d5834d32702635013ae10d8e458dc11a0af9
NandoCruz <nando33_2005@hotmail.com>
2021-04-15 20:57:32 -0300

    aula 11 de JavaScript

---
+++ Aula11/exc08.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade de seu veículo é ${vel} km/h`)
+if ( vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Use sempre o cinto de segurança.`)
