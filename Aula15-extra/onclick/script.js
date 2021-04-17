1fbc00234d63d9dc86081c783554c4398f3ec464
NandoCruz <nando33_2005@hotmail.com>
2021-04-17 20:36:59 -0300

    adicionando exercício extra

---
+++ Aula15-extra/onclick/script.js
@@ -0,0 +1,5 @@
+function ok() {
+    prompt('Qual o seu nome:')
+
+    alert('você está estudando javaScript')
+}
--- Aula15-extra/script.js
+++ Aula15-extra/script.js
@@ -1,3 +1,7 @@
 function verificar() {
-
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nome = window.document.getElementById("nome")
+    var fano = window.document.getElementById("ano")
+    lert = (`Olá, ${nome}! você nasceu no ano de ${fano}`)
 }
