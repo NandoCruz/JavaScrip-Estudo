d03c1a8ba3b6b0a6c28d5c8eb874d0f1f3d3c52d
NandoCruz <nando33_2005@hotmail.com>
2021-04-17 17:52:58 -0300

    adicionando aula extra

--- Aula13/script.js
+++ Aula13/script.js
@@ -1,16 +1,17 @@
-function carregar(){
+function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
     var hora = data.getHours()
-    // var hora = 19
-    msg.innerHTML = `Agora são: ${hora} horas.`
-    if (hora >= 0 && hora < 12){
+    var min = data.getMinutes()
+        // var hora = 19
+    msg.innerHTML = `Agora são: ${hora}:${min} horas.`
+    if (hora >= 0 && hora < 12) {
         var dia = window.document.getElementById('titulo')
         dia.innerHTML = `Bom dia!`
         img.src = 'manha.png'
         document.body.style.background = '#c98638'
-    } else if (hora >= 12 && hora < 18){
+    } else if (hora >= 12 && hora < 18) {
         var tarde = window.document.getElementById('titulo')
         tarde.innerHTML = 'Boa tarde!'
         img.src = 'tarde.png'
