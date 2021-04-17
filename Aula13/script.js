c1dc28bd28d7360b16b98d3f9749dafe77b6061b
NandoCruz <nando33_2005@hotmail.com>
2021-04-16 21:20:34 -0300

    adicionando aula 14

--- Aula13/script.js
+++ Aula13/script.js
@@ -2,9 +2,9 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 10
-    msg.innerHTML = `Agora são ${hora} horas`
+    var hora = data.getHours()
+    // var hora = 19
+    msg.innerHTML = `Agora são: ${hora} horas.`
     if (hora >= 0 && hora < 12){
         var dia = window.document.getElementById('titulo')
         dia.innerHTML = `Bom dia!`
