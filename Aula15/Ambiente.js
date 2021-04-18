0f9dec542f4574663defb76fad00c345e4f5e080
NandoCruz <nando33_2005@hotmail.com>
2021-04-18 16:15:27 -0300

    estudando while
    Estudando o sistema de repetição while e do while

---
+++ Aula15/Ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`contar ${c}`)
+    c++
+}
+while (c <= 6)
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+
+var c = 1
+while (c <= 6) {
+    console.log('tudo Bem?')
+    c++
+}*/
