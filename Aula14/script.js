83adc77a97ca488bc4774a555ddd6a1b72170ee5
NandoCruz <nando33_2005@hotmail.com>
2021-04-17 22:17:06 -0300

    Ajustando o Checked
    Ajustando o comando checked de homem ou mulher

--- Aula14/script.js
+++ Aula14/script.js
@@ -6,16 +6,15 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value > ano)) {
         window.alert('[ERRRO!] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByTagName('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var genero = ''
+        var genero = ""
 
         if (fsex[0].checked) {
             genero = 'Homem'
         } else if (fsex[1].checked) {
             genero = 'Mulher'
         }
-
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
 
     }
