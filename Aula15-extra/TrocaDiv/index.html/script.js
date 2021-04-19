e0198b439ea650fe3bb6b58d267b197f90676e3f
NandoCruz <nando33_2005@hotmail.com>
2021-04-19 19:53:57 -0300

    Criando a pasta trocaDiv
    movendo os arquivos: index, css e js para dentro da pasta modelo e criando a pasta TrocaDiv

---
+++ Aula15-extra/TrocaDiv/index.html/script.js
@@ -0,0 +1,6 @@
+function alterar() {
+    // alert('Tudo certo até aqui!')
+    var nome = window.document.getElementById('nome')
+    var res = window.document.getElementById('res')
+    res.innerHTML = `Seu nome é ${res}.`
+}
