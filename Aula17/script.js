03b09642409136323599492f955e0acf9da62e69
NandoCruz <nando33_2005@hotmail.com>
2021-04-19 23:59:36 -0300

    ajustando o JavaScript

--- Aula17/script.js
+++ Aula17/script.js
@@ -1,18 +1,34 @@
 function contar() {
-    var txn1 = window.document.getElementById("txtn1")
-    var txn2 = window.document.getElementById("txtn2")
-    var passe = window.document.getElementById("passe")
+    var ini = window.document.getElementById("txtn1")
+    var fim = window.document.getElementById("txtn2")
+    var passo = window.document.getElementById("passo")
     var res = window.document.querySelector("div#res")
-    var n1 = Number(txn1.value)
-    var n2 = Number(txn2.value)
-    var c = Number(passe.value)
-        // alert(`${n1}, ${n2} e ${c}`)
-    if (txtn1.value == 0 || txtn2.value == 0) {
-        alert(`[ERRO!] Digite um número.`)
-    }
-    while (n1 <= n2) {
-        alert(`${c}`)
-        c++
-    }
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO!] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando... <br>'
+
+        var n1 = Number(ini.value)
+        var n2 = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO = 1')
+            p = 1
+        }
+
+        if (n1 < n2) {
+            // Contagem Crescente
+            for (var c = n1; c <= n2; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem decrescente
+            for (var c = n1; c >= n2; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
+    }
 }
