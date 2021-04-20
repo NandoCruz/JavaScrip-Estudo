b010e339c000502e9f212fc570c84658c8cae6c3
NandoCruz <nando33_2005@hotmail.com>
2021-04-20 20:30:16 -0300

    criando as teclas

--- Aula15-extra/Calculadora/script.js
+++ Aula15-extra/Calculadora/script.js
@@ -1,21 +1,88 @@
-var btn01 = window.document.getElementById('btn1')
-var btn02 = window.document.getElementById('btn2')
-var btn03 = window.document.getElementById('btn3')
-var res = window.document.getElementById('txtnd')
-var n1 = Number(btn01.value)
-var n2 = Number(btn02.value)
-var n3 = Number(btn03)
-
-
-function somar() {
-    var btn01 = window.document.getElementById('btn1')
-    var btn02 = window.document.getElementById('btn2')
-    var btn03 = window.document.getElementById('btn3')
-    var res = window.document.getElementById('txtnd')
-    var n1 = Number(btn01.value)
-    var n2 = Number(btn02.value)
-    var n3 = Number(btn03)
-
-    var s = n1 + n2
+function button0() {
+    let btn0 = window.document.getElementById('btn0')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n0 = Number(btn0.value)
+    mostra.innerHTML = `${n0}`
 }
-res.innerHTML = `${s}`
+
+function button1() {
+    let btn01 = window.document.getElementById('btn1')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n1 = Number(btn01.value)
+    mostra.innerHTML = `${n1}`
+}
+
+function button2() {
+    let btn02 = window.document.getElementById('btn2')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n2 = Number(btn02.value)
+    mostra.innerHTML = `${n2}`
+}
+
+function button3() {
+    let btn03 = window.document.getElementById('btn3')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n3 = Number(btn03.value)
+    mostra.innerHTML = `${n3}`
+}
+
+function button4() {
+    let btn04 = window.document.getElementById('btn4')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n4 = Number(btn04.value)
+    mostra.innerHTML = `${n4}`
+}
+
+function button5() {
+    let btn05 = window.document.getElementById('btn5')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n5 = Number(btn05.value)
+    mostra.innerHTML = `${n5}`
+}
+
+function button6() {
+    let btn06 = window.document.getElementById('btn6')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n6 = Number(btn06.value)
+    mostra.innerHTML = `${n6}`
+}
+
+function button7() {
+    let btn07 = window.document.getElementById('btn7')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n7 = Number(btn07.value)
+    mostra.innerHTML = `${n7}`
+}
+
+function button8() {
+    let btn08 = window.document.getElementById('btn8')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n8 = Number(btn08.value)
+    mostra.innerHTML = `${n8}`
+}
+
+function button9() {
+    let btn09 = window.document.getElementById('btn9')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let n9 = Number(btn09.value)
+    mostra.innerHTML = `${n9}`
+}
+
+function buttonC() {
+    let btnC = window.document.getElementById('btnC')
+    let mostra = window.document.querySelector('div#res p')
+    let res = Number(mostra.value)
+    let C = Number(btnC.value)
+    C = 0
+    mostra.innerHTML = `${C}`
+}
