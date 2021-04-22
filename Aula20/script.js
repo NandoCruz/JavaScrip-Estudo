e39905546703d9ffd7fb54c8ab7990e056aa6adb
NandoCruz <nando33_2005@hotmail.com>
2021-04-21 22:10:03 -0300

    finalizando a aula

--- Aula20/script.js
+++ Aula20/script.js
@@ -1,21 +1,63 @@
-function adiciona() {
-    var txtnum1 = window.document.getElementById('txtnum')
-    var res = window.document.getElementById('res')
-    var n = Number(txtnum1.value)
-    if (n == '' || n > 100) {
-        alert('Valor inválido ou já adicionado na lista.')
+let num = window.document.querySelector('input#txtnum')
+let res = window.document.querySelector('div#res')
+let lista = window.document.querySelector('select#lista')
+let valor = []
+
+function isNum(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        res.innerHTML = `Valor ${n} Adicionado`
+        return false
     }
 }
 
-function finaliza() {
-    var txtnum1 = window.document.getElementById('txtnum')
-    var res = window.document.getElementById('res')
-    var n = Number(txtnum1.value)
-    if (n == '' || n > 100) { //alterar a condição para o vetor
-        alert('Valor inválido ou já adicionado na lista.')
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
     } else {
-        res.innerHTML = `Ao todo tem ${}`
+        return false
     }
+}
+
+function adicionar() {
+    if (isNum(num.value) && !inLista(num.value, valor)) {
+        valor.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já adicionado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valor.length == 0) {
+        alert('Adicionar valores antes de finalizar!')
+    } else {
+        let tot = valor.length
+        let maior = valor[0]
+        let menor = valor[0]
+        let soma = valor[0]
+        letmedia = valor[0]
+        for (let pos in valor) {
+            soma += valor[pos]
+            if (valor[pos] > maior) {
+                maior = valor[pos]
+            } else
+            if (valor[pos] < menor) {
+                menor = valor[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo tem <strong>${tot}</strong> Elementos cadastrados </p>`
+        res.innerHTML += `<p>O valor maior informado <strong>${maior}</strong> </p>`
+        res.innerHTML += `<p> O valor menor informado <strong>${menor}</strong> </p>`
+        res.innerHTML += `<p> Somando todos os valores temos <strong>${soma}</strong> </p>`
+        res.innerHTML += `<p> A média de todos os valores digitados é ${media} </p>`
+    }
+
 }
